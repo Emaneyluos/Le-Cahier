54fd9ec915f09776ff0d4d37a6aa171c28e55697
Emaneyluos <benxxix@gmail.com>
2023-12-19 09:56:48 +0400

    Version test du css de home

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     var burger = document.querySelector('.burger');
     var sidebar = document.querySelector('.sidebar');
-    var container = document.querySelector('.container');
 
     burger.addEventListener('click', function () {
         sidebar.classList.toggle('active');
-        container.classList.toggle('active');
         burger.classList.toggle('active');
     });
 });
